3952f57ea873c29b107650f8536a61d857258bb6
kyj0503 <heroria0503@gmail.com>
2024-03-19 17:13:09 +0900

    자바스크립트 공부

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-// import AppComp02 from './src/AppComp02'
-
-// 태그 사이에 중괄호 있으면 자바스크립트
-// === 타입까지 같은지 확인
-
-// 
-
 export default function App() {
-  return <AppComp02 />
+
+  const name = 'hongildong';
+
+  const MyButton = () => {
+    return 
+  }
+
+  return (
+    <View style={StyleSheet.container}>
+      <Text>
+        My name is {name === 'hanbit' ? 'hanbit' : (name === 'hongildong' ? 'hongildong' : 'React Native')}
+      </Text>
+      <StatusBar style="auto" />
+    </View>)
 }
