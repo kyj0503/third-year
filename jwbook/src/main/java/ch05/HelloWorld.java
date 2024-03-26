6a5e070f42347c31eab403181fa7e8289d4fb965
kyj0503 <heroria0503@gmail.com>
2024-03-26 14:18:19 +0900

    챕터5 학습내용

---
+++ jwbook/src/main/java/ch05/HelloWorld.java
@@ -0,0 +1,44 @@
+package ch05;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet(description = "MyFirstServelt", urlPatterns = {"/hello"})
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("Text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.append("<!doctype html><html><head><title>Hello World Servelt</title></head><body>")
+			.append("<h2>Hello World !!</h2><hr>")
+			.append("현재 날짜와 시간은: "+java.time.LocalDateTime.now())
+			.append("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
