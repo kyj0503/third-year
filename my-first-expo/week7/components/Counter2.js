bfbb7f585bea699cde577decddc4c1287cb50768
kyj0503 <heroria0503@gmail.com>
2024-04-16 15:36:47 +0900

    학습 내용

---
+++ my-first-expo/week7/components/Counter2.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import {View, Text} from 'react-native'
+import styled from 'styled-components/native'
+import Button from './Button'
+
+const StyledText = styled.Text`
+    font-size: 24px;
+    margin: 10px;
+`
+
+//arrow 함수를 이용한 setter 함수 호출
+const Counter = () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+export default Counter
--- my-first-expo/week7/theme.js
+++ my-first-expo/week7/theme.js
@@ -1,29 +1,6 @@
-//스타일 객체
-//직접적인 색상보다는 개념적인 이름을 사용
 
-//theme object
-export const theme = {
+export const theme = {  
     purple: '#9b59b6',
-    blue: '#3498db'    
+    blue: '#3498db',
+    white: '#fff'  
 };
-
-//light테마
-export const lightTheme = {
-    background: '#fff',
-    text: '#fff',
-    purple: '#9b59b6',
-    blue: '#3498db'
-}
-
-//dark 테마
-export const darkTheme = {
-    background: '#34495e',
-    text: '#34495e',
-    purple: '#9b59b6',
-    blue: '#3498db'
-}
-
-
-
-
-
