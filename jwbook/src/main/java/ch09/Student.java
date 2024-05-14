87111ede78125f93e46c88f72c470b0d577c706e
kyj0503 <heroria0503@gmail.com>
2024-05-14 13:34:12 +0900

    JDBC 공부 내용

---
+++ jwbook/src/main/java/ch09/Student.java
@@ -0,0 +1,51 @@
+package ch09;
+import java.sql.Date;
+
+public class Student {
+	private int id;
+	private String username;
+	private String univ;
+	private Date birth;
+	private String email;
+	public void setId(int int1) {
+		// TODO Auto-generated method stub
+		
+	}
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void setUsername(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public String getUniv() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void setUniv(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public Date getBirth() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void setBirth(Date date) {
+		// TODO Auto-generated method stub
+		
+	}
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void setEmail(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	
+
+
+}
