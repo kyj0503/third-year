764405aeb9bde2037f17f6616b6f68957d16ec5e
kyj0503 <heroria0503@gmail.com>
2024-05-19 22:41:16 +0900

    과제 학습 내용

--- tmp.py
+++ tmp.py
@@ -6,6 +6,7 @@ from common.gradient import numerical_gradient
 from collections import OrderedDict
 from dataset.mnist import load_mnist
 import matplotlib.pyplot as plt
+from common.optimizer import Adam
 
 class MultiLayerNet:
     def __init__(self, input_size, hidden_size, output_size):
@@ -29,46 +30,31 @@ class MultiLayerNet:
         self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
         self.layers['BatchNorm1'] = BatchNormalization(self.params['gamma1'], self.params['beta1'])
         self.layers['Relu1'] = Relu()
+        self.layers['Dropout1'] = Dropout(0.5)
         self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
         self.layers['BatchNorm2'] = BatchNormalization(self.params['gamma2'], self.params['beta2'])
         self.layers['Relu2'] = Relu()
+        self.layers['Dropout2'] = Dropout(0.5)
         self.layers['Affine3'] = Affine(self.params['W3'], self.params['b3'])
 
         self.lastLayer = SoftmaxWithLoss()
-        
+
     def predict(self, x):
         for layer in self.layers.values():
             x = layer.forward(x)
-        
         return x
-        
-    # x : 입력 데이터, t : 정답 레이블
+
     def loss(self, x, t):
         y = self.predict(x)
         return self.lastLayer.forward(y, t)
-    
+
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis=1)
         if t.ndim != 1 : t = np.argmax(t, axis=1)
-        
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
-        
-    # x : 입력 데이터, t : 정답 레이블
-    def numerical_gradient(self, x, t):
-        loss_W = lambda W: self.loss(x, t)
-        
-        grads = {}
-        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
-        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
-        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
-        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
-        grads['W3'] = numerical_gradient(loss_W, self.params['W3'])
-        grads['b3'] = numerical_gradient(loss_W, self.params['b3'])
-        
-        return grads
-        
+
     def gradient(self, x, t):
         # forward
         self.loss(x, t)
@@ -76,7 +62,7 @@ class MultiLayerNet:
         # backward
         dout = 1
         dout = self.lastLayer.backward(dout)
-        
+
         layers = list(self.layers.values())
         layers.reverse()
         for layer in layers:
@@ -89,18 +75,19 @@ class MultiLayerNet:
         grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
         grads['gamma2'], grads['beta2'] = self.layers['BatchNorm2'].dgamma, self.layers['BatchNorm2'].dbeta
         grads['W3'], grads['b3'] = self.layers['Affine3'].dW, self.layers['Affine3'].db
-        
+
         return grads
 
 # 데이터 읽기
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-network = MultiLayerNet(input_size=784, hidden_size=10, output_size=10) # hidden_size 변경
+network = MultiLayerNet(input_size=784, hidden_size=15, output_size=10)
 
-iters_num = 10000 # 반복 횟수는 10000번
+# 하이퍼파라미터
+iters_num = 10000  # 반복 횟수를 적절히 설정한다.
 train_size = x_train.shape[0]
-batch_size = 100
-learning_rate = 0.01 # learning_rate 변경
+batch_size = 100   # 미니배치 크기
+learning_rate = 0.019
 
 train_loss_list = []
 train_acc_list = []
@@ -108,33 +95,29 @@ test_acc_list = []
 
 iter_per_epoch = max(train_size / batch_size, 1)
 
+optimizer = Adam()
+
 for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
-    
+
     # 기울기 계산
-    grad = network.gradient(x_batch, t_batch) # 오차역전파법 방식
-    
-    # 갱신
-    for key in ('W1', 'b1', 'gamma1', 'beta1', 'W2', 'b2', 'gamma2', 'beta2', 'W3', 'b3'):
-        network.params[key] -= learning_rate * grad[key]
-    
-    loss = network.loss(x_batch, t_batch)
-    train_loss_list.append(loss)
-    
+    grad = network.gradient(x_batch, t_batch)
+    optimizer.update(network.params, grad)
+
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_train, t_train)
         test_acc = network.accuracy(x_test, t_test)
         train_acc_list.append(train_acc)
         test_acc_list.append(test_acc)
-        print(train_acc, test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
 
 # 그래프 그리기
 markers = {'train': 'o', 'test': 's'}
 x = np.arange(len(train_acc_list))
-plt.plot(x, train_acc_list, label='train acc')
-plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.plot(x, train_acc_list, marker='o', label='train', markevery=10)
+plt.plot(x, test_acc_list, marker='s', label='test', markevery=10)
 plt.xlabel("epochs")
 plt.ylabel("accuracy")
 plt.ylim(0, 1.0)
