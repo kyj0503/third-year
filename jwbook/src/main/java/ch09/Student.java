2fbbd5950cc718f0913d640d1fbff54d70bbaa45
kyj0503 <heroria0503@gmail.com>
2024-05-21 17:05:47 +0900

    10주차

--- jwbook/src/main/java/ch09/Student.java
+++ jwbook/src/main/java/ch09/Student.java
@@ -1,51 +1,51 @@
 package ch09;
+
 import java.sql.Date;
 
 public class Student {
-	private int id;
-	private String username;
-	private String univ;
-	private Date birth;
-	private String email;
-	public void setId(int int1) {
-		// TODO Auto-generated method stub
-		
-	}
-	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public void setUsername(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-	public String getUniv() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public void setUniv(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-	public Date getBirth() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public void setBirth(Date date) {
-		// TODO Auto-generated method stub
-		
-	}
-	public String getEmail() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public void setEmail(String string) {
-		// TODO Auto-generated method stub
-		
-	}
+    private int id;
+    private String username;
+    private String univ;
+    private Date birth;
+    private String email;
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	
+    public int getId() {
+        return id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUniv() {
+        return univ;
+    }
+
+    public void setUniv(String univ) {
+        this.univ = univ;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- jwbook/src/main/java/ch09/StudentController.java
+++ jwbook/src/main/java/ch09/StudentController.java
@@ -1,17 +1,21 @@
 package ch09;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
-import org.apache.commons.beanutils.Beanutils;
+import org.apache.commons.beanutils.BeanUtils;
 
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 @WebServlet("/studentControl")
-public class StudentController {
+public class StudentController extends HttpServlet{
+	@SuppressWarnings("unused")
 	private static final long serialVerionUID = 1L;
 	
 	StudentDAO dao;
@@ -23,30 +27,48 @@ public class StudentController {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getParameter("action");
 		String view = "";
-		if(request.getParameter("action") == null ) {
+		if(action == null) {
 			getServletContext().getRequestDispatcher("/studentControl?action=list").forward(request, response);
+			return;
 		} else {
 			switch(action) {
 			case "list": view = list(request, response); break;
 			case "insert": view = insert(request, response); break;
+			case "delete": view = delete(request, response); break;
 			}
 			
 			getServletContext().getRequestDispatcher("/ch09/"+view).forward(request, response);
 		}
 	}
 
+	private String delete(HttpServletRequest request, HttpServletResponse response) {
+	    int id = Integer.parseInt(request.getParameter("id"));
+	    try {
+	        dao.delete(id);
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        request.setAttribute("errorMessage", "학생 정보 삭제 중 오류가 발생했습니다.");
+	        return "error.jsp";
+	    }
+	    return list(request, response);
+	}
+
+	// list, add view (studentInfo.jsp)로 이동
 	public String list(HttpServletRequest request, HttpServletResponse response) {
+		// getAll
 		request.setAttribute("students", dao.getAll());
 		return "studentInfo.jsp";
 	}
 	
+	// Student 정보 기입 후 studentInfo 보여주기
 	public String insert(HttpServletRequest request, HttpServletResponse response) {
 		Student s = new Student();
 		try {
-			Beanutils.populate(s, request.getParameterMap());
+			BeanUtils.populate(s, request.getParameterMap());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		System.out.println(s);
 		dao.insert(s);
 		return list(request, response);
 	}
