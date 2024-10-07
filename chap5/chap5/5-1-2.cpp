3a0714d03c4d3341a6a10bbc250389394d152011
kyj0503 <heroria0503@gmail.com>
2024-10-07 18:19:09 +0900

    5챕터 학습 내용

---
+++ chap5/chap5/5-1-2.cpp
@@ -0,0 +1,19 @@
+// 인자 전달 방식 - 주소에 의한 호출
+
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b) {
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int main() {
+	int m = 2, n = 9;
+	swap(&m, &n);
+	cout << m << ' ' << n;
+	return 0;
+}
