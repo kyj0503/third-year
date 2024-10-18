7a60ea6db2b8163b33576898db93b57a3f2f62b3
kyj0503 <heroria0503@gmail.com>
2024-10-18 22:51:31 +0900

    과제3

--- HW3-1.cpp
+++ HW3-1.cpp
@@ -32,7 +32,7 @@ Dept::~Dept() {
 }
 
 void Dept::read() {
-    cout << size << "°³ Á¡¼ö ÀÔ·Â>> ";
+    cout << size << "ê°œ ì ìˆ˜ ìž…ë ¥>> ";
     for (int i = 0; i < size; i++) {
         cin >> scores[i];
     }
@@ -59,5 +59,5 @@ int main() {
     Dept com(10);
     com.read();
     int n = countPass(com);
-    cout << "60Á¡ ÀÌ»óÀº " << n << "¸í";
+    cout << "60ì  ì´ìƒì€ " << n << "ëª…";
 }
--- HW3-3.cpp
+++ HW3-3.cpp
@@ -30,7 +30,7 @@ int countPass(Dept& dept) {
     return count;
 }
 void Dept::read() {
-    cout << size << "°³ Á¡¼ö ÀÔ·Â>> ";
+    cout << size << "ê°œ ì ìˆ˜ ìž…ë ¥>> ";
     for (int i = 0; i < size; i++) {
         cin >> scores[i];
     }
@@ -48,5 +48,5 @@ int main() {
     Dept com(10);
     com.read();
     int n = countPass(com);
-    cout << "60Á¡ ÀÌ»óÀº " << n << "¸í";
+    cout << "60ì  ì´ìƒì€ " << n << "ëª…";
 }
