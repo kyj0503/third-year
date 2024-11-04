4fc7c99b61f81e646878ac59da138a582645dba9
kyj0503 <heroria0503@gmail.com>
2024-11-04 21:49:44 +0900

    할 일이 많다

--- src/main/java/com/example/kproject/repository/ReviewRepository.java
+++ src/main/java/com/example/kproject/repository/ReviewRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findByPlace_PlaceIdOrderByRatingDesc(Long placeId);
+    List<Review> findAllByOrderByRatingDesc();
 }
