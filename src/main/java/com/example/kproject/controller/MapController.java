74f8f268305fa2f7c1a2516b6d27316c2585472e
kyj0503 <heroria0503@gmail.com>
2024-11-05 17:13:28 +0900

    움직일때마다 불러오기 기능 추가

--- src/main/java/com/example/kproject/controller/MapController.java
+++ src/main/java/com/example/kproject/controller/MapController.java
@@ -1,27 +1,23 @@
 package com.example.kproject.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * 컨트롤러에서 API 키를 모델에 추가하여 뷰로 전달
- * 
- * @author 김연재
- */
-
 @Controller
 public class MapController {
 
-    private final String kakaoApiKey;
+    @Value("${kakao.api.key}")
+    private String kakaoApiKey;
 
-    public MapController(String kakaoApiKey) {
-        this.kakaoApiKey = kakaoApiKey;
-    }
+    private static final String DEFAULT_KEYWORD = "카페";
 
     @GetMapping("/")
     public String showMap(Model model) {
+        // Kakao API 키와 검색 키워드를 모델에 추가하여 템플릿에 전달
         model.addAttribute("kakaoApiKey", kakaoApiKey);
+        model.addAttribute("keyword", DEFAULT_KEYWORD);
         return "map";
     }
 }
