249f3d7c03ef2359c0fbb132412563f45a36442c
kyj0503 <heroria0503@gmail.com>
2025-01-10 10:40:29 +0900

    최초 커밋

---
+++ src/test/java/com/webproject/kangneng_back/KangnengBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webproject.kangneng_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KangnengBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
