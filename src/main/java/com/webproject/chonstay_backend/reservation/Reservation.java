42cb492a380911b4e44812dea54cef37af31f725
jjy12163 <>
2025-01-19 02:33:39 +0900

    [FEAT] Reservation도메인

---
+++ src/main/java/com/webproject/chonstay_backend/reservation/Reservation.java
@@ -0,0 +1,38 @@
+package com.webproject.chonstay_backend.reservation;
+
+import com.webproject.chonstay_backend.home.Home;
+import com.webproject.chonstay_backend.user.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long reservationId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "home_id", nullable = false)
+    private Home home;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDate checkInDate;
+
+    @Column(nullable = false)
+    private LocalDate checkoutDate;
+
+    //enum
+    @Column(nullable = false)
+    private Enum status;
+
+    @Column(nullable = false)
+    private short guestNumber;
+
+}
