d03039575ae6bf5737478e7e91e1f851c5dffda6
nakyeong <rinarina0429@naver.com>
2025-01-19 06:24:39 +0900

    [FEAT] 숙소 검색 필터링 및 전체 조회 기능 구현

--- src/main/java/com/webproject/chonstay_backend/home/HomeController.java
+++ src/main/java/com/webproject/chonstay_backend/home/HomeController.java
@@ -2,12 +2,15 @@ package com.webproject.chonstay_backend.home;
 
 import static org.springframework.http.HttpStatus.OK;
 
+import com.webproject.chonstay_backend.common.Location;
 import com.webproject.chonstay_backend.home.dto.HomeGetResponse;
+import com.webproject.chonstay_backend.home.dto.HomesGetResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,5 +26,15 @@ public class HomeController {
         return ResponseEntity
                 .status(OK)
                 .body(homeService.getHome(homeId));
+    }
+
+    @GetMapping
+    public ResponseEntity<HomesGetResponse> getHomes(
+            @RequestParam(required = false) String searchQuery,
+            @RequestParam(required = false) Location location
+    ) {
+        return ResponseEntity
+                .status(OK)
+                .body(homeService.getResultHomes(searchQuery, location));
     }
 }
