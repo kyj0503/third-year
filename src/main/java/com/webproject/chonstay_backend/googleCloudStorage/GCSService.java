062cefdd952019c3144f64a8adf3928e9dc14a6c
kyj0503 <heroria0503@gmail.com>
2025-01-19 00:07:26 +0900

    FEAT: Google Cloud Storage에 이미지 저장 기능 구현

---
+++ src/main/java/com/webproject/chonstay_backend/googleCloudStorage/GCSService.java
@@ -0,0 +1,40 @@
+package com.webproject.chonstay_backend.googleCloudStorage;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class GCSService {
+
+    private final Storage storage;
+
+    @Value("${google.cloud.storage.bucket-name}")
+    private String bucketName;
+
+    public GCSService(@Value("${google.cloud.storage.key-file}") String keyFilePath) throws IOException {
+        this.storage = StorageOptions.newBuilder()
+                .setCredentials(GoogleCredentials.fromStream(getClass().getClassLoader().getResourceAsStream(keyFilePath)))
+                .build()
+                .getService();
+    }
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+
+        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, fileName)
+                .setContentType(file.getContentType())
+                .build();
+
+        storage.create(blobInfo, file.getBytes());
+
+        return String.format("https://storage.googleapis.com/%s/%s", bucketName, fileName);
+    }
+}
