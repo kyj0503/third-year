57884f639256feec366841d73f634d6e20743569
kyj0503 <heroria0503@gmail.com>
2025-01-19 01:24:32 +0900

    yaml 의존성 삭제

--- src/main/java/com/webproject/chonstay_backend/googleCloudStorage/GCSService.java
+++ src/main/java/com/webproject/chonstay_backend/googleCloudStorage/GCSService.java
@@ -4,7 +4,6 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,20 +15,19 @@ import java.util.UUID;
 public class GCSService {
 
     private final Storage storage;
-
     private final String bucketName;
 
-    public GCSService(
-            @Value("${google.cloud.storage.key-file:}") String keyFilePathFromYaml,
-            @Value("${google.cloud.storage.bucket-name:}") String bucketNameFromYaml
-    ) throws IOException {
-        // 배포 환경에서는 환경 변수에서 값을 가져오고, 로컬 환경에서는 YAML에서 가져오도록 설정
-        String keyFilePath = System.getenv("GCS_KEY_FILE") != null ? System.getenv("GCS_KEY_FILE") : keyFilePathFromYaml;
-        this.bucketName = System.getenv("GCS_BUCKET_NAME") != null ? System.getenv("GCS_BUCKET_NAME") : bucketNameFromYaml;
+    public GCSService() throws IOException {
+        // 환경 변수에서 값을 읽음
+        String keyFilePath = System.getenv("GCS_KEY_FILE");
+        this.bucketName = System.getenv("GCS_BUCKET_NAME");
 
-        // 키 파일이 없으면 예외를 발생시킴
+        // 키 파일과 버킷 이름 확인
         if (keyFilePath == null || keyFilePath.isEmpty()) {
             throw new IllegalArgumentException("Google Cloud Storage key file path is not provided.");
+        }
+        if (this.bucketName == null || this.bucketName.isEmpty()) {
+            throw new IllegalArgumentException("Google Cloud Storage bucket name is not provided.");
         }
 
         // GoogleCredentials를 사용하여 인증
