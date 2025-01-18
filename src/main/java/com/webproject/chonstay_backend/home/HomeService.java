9fd7558cac2c7d5633279f8572e3e74fc29f1bb8
nakyeong <rinarina0429@naver.com>
2025-01-19 03:13:10 +0900

    [FEAT] Controller 관련 class 생성

---
+++ src/main/java/com/webproject/chonstay_backend/home/HomeService.java
@@ -0,0 +1,11 @@
+package com.webproject.chonstay_backend.home;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class HomeService {
+}
