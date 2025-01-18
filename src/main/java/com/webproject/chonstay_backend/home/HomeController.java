c7d18c106ebae668cd39a3f7ab3eb836d1376c41
nakyeong <rinarina0429@naver.com>
2025-01-19 05:01:09 +0900

    [FEAT] 숙소 단건 조회 기능

--- src/main/java/com/webproject/chonstay_backend/home/HomeController.java
+++ src/main/java/com/webproject/chonstay_backend/home/HomeController.java
@@ -1,11 +1,27 @@
 package com.webproject.chonstay_backend.home;
 
+import static org.springframework.http.HttpStatus.OK;
+
+import com.webproject.chonstay_backend.home.dto.HomeGetResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/homes")
 @RequiredArgsConstructor
 public class HomeController {
+
+    private final HomeService homeService;
+
+    @GetMapping("/{homeId}")
+    public ResponseEntity<HomeGetResponse> getHome(@PathVariable Long homeId) {
+
+        return ResponseEntity
+                .status(OK)
+                .body(homeService.getHome(homeId));
+    }
 }
