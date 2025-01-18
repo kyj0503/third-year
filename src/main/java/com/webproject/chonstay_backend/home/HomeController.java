84344ff0cee50ed8428278b7551afda608f7d03b
jjy12163 <>
2025-01-19 04:56:00 +0900

    숙소 등록

--- src/main/java/com/webproject/chonstay_backend/home/HomeController.java
+++ src/main/java/com/webproject/chonstay_backend/home/HomeController.java
@@ -1,6 +1,9 @@
 package com.webproject.chonstay_backend.home;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 @RequiredArgsConstructor
 public class HomeController {
+
+    private final HomeService homeService;
+
+    @PostMapping("/home")
+    public ResponseEntity<String> registerHome(@RequestBody HomeRegisterRequestDto requestDto) {
+        homeService.registerHome(requestDto); // 서비스 계층에서 등록 처리
+        return ResponseEntity.ok("Home successfully registered!");
+    }
+
+
 }
--- src/main/java/com/webproject/chonstay_backend/home/HomeService.java
+++ src/main/java/com/webproject/chonstay_backend/home/HomeService.java
@@ -1,6 +1,7 @@
 package com.webproject.chonstay_backend.home;
 
 import lombok.RequiredArgsConstructor;
+import org.json.JSONException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +9,36 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class HomeService {
+
+    private final GeoLocationService geoLocationService;
+    private final HomeRepository homeRepository;
+
+    //@param requestDto HomeRegisterRequestDto
+
+    @Transactional
+    public void registerHome(HomeRegisterRequestDto requestDto) {
+        // DTO를 엔티티로 변환
+        Home home = new Home();
+        home.setHomeName(requestDto.getHomeName());
+        home.setAddress(requestDto.getAddress());
+        home.setMaxGuest(requestDto.getMaxGuest());
+        home.setDescription(requestDto.getDescription());
+        home.setHomeImageUrl(requestDto.getHomeImageUrl());
+        home.setHomeStatus(requestDto.getHomeStatus());
+
+        // 주소가 제공된 경우, Geocoding API를 통해 위도와 경도 설정
+        if (requestDto.getAddress() != null && !requestDto.getAddress().isEmpty()) {
+
+            try {
+                geoLocationService.setCoordinatesFromAddress(home, requestDto.getAddress());
+            } catch (JSONException e) {
+                // 예외 처리 (예: 로깅, 사용자에게 피드백 제공)
+                e.printStackTrace();
+            }
+            // GeoLocationService.setCoordinatesFromAddress(home, requestDto.getAddress());
+        }
+
+        // 엔티티 저장
+        homeRepository.save(home);
+    }
 }
