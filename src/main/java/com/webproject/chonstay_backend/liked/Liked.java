6472fe4b383525c60f5e6576153b8b852e42dec4
jjy12163 <>
2025-01-19 02:33:39 +0900

    [FEAT] Reservation도메인

---
+++ src/main/java/com/webproject/chonstay_backend/liked/Liked.java
@@ -0,0 +1,28 @@
+package com.webproject.chonstay_backend.liked;
+
+import com.webproject.chonstay_backend.home.Home;
+import com.webproject.chonstay_backend.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Liked {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long likedHomeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "home_id", nullable = false)
+    private Home home;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
---
+++ src/main/java/com/webproject/chonstay_backend/reservation/Status.java
@@ -0,0 +1,7 @@
+package com.webproject.chonstay_backend.reservation;
+
+public enum Status {
+    // '예약 가능', '확인 중', '예약 완료', '예약 취소'
+    USABLE, CHECKING, CHECKED, CANCELED
+
+}
