73689b4c81a011e5ecf9413d6a2014a397893247
kyj0503 <heroria0503@gmail.com>
2025-01-19 02:00:39 +0900

    이미지 업로드 기능 테스트

--- src/main/java/com/webproject/chonstay_backend/googleCloudStorage/GCSController.java
+++ src/main/java/com/webproject/chonstay_backend/googleCloudStorage/GCSController.java
@@ -1,15 +1,21 @@
 package com.webproject.chonstay_backend.googleCloudStorage;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
+
 @RestController
 @RequestMapping("/api/images")
 public class GCSController {
+
     private final GCSService gcsService;
+
     public GCSController(GCSService gcsService) {
         this.gcsService = gcsService;
     }
+
     @PostMapping("/upload")
     public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
         try {
