76056853ad49c2cdc3d2a7190b7627d0c109dd75
kyj0503 <heroria0503@gmail.com>
2025-01-18 18:08:34 +0900

    feat: 최초 커밋

---
+++ src/main/java/com/webproject/chonstay_backend/ChonstayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.chonstay_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChonstayBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChonstayBackendApplication.class, args);
+    }
+
+}
