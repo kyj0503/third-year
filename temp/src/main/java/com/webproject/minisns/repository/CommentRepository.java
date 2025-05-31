6ae805211a25b42586b510789352618afb653659
kyj0503 <heroria0503@gmail.com>
2025-06-01 03:33:48 +0900

    파일 삭제

--- temp/src/main/java/com/webproject/minisns/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.webproject.minisns.repository;
-
-import com.webproject.minisns.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
--- temp/src/main/java/com/webproject/minisns/repository/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.webproject.minisns.repository;
-
-import com.webproject.minisns.entity.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
-}
